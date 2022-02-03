f2bbb41ca1d9d9b62af4385b89f7f7d1dc12bc25
Danilo D'Amico <45599533+DaniloDamico@users.noreply.github.com>
2022-02-03 09:55:48 +0100

    project skeleton

---
+++ src/com/pattern/Main.java
@@ -0,0 +1,9 @@
+package com.pattern;
+
+public class Main { // Client Class
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
